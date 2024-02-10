82c78bb7d945ce764e34cb9e948432a0cea4f2fc
Hao Le <hao@lemang.llc>
2024-02-10 13:35:33 -0800

    add basic line item views

--- app/models/time_sheet.rb
+++ app/models/time_sheet.rb
@@ -2,7 +2,7 @@ class TimeSheet < ApplicationRecord
   belongs_to :user
   has_many :line_items, dependent: :destroy
 
-  accepts_nested_attributes_for :line_items, allow_destroy: true
+  accepts_nested_attributes_for :line_items, allow_destroy: true, reject_if: :all_blank
 
   def total_time
    line_items.sum(:minutes)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :time_sheets
+  resources :time_sheets do
+    resources :line_items
+  end
   resources :users do
     resources :time_sheets do
       resources :line_items
